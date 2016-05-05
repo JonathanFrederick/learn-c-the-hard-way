47a4785a86c1bae9d7da38a528514e898e84c8f9
Jonathan Frederick <jrfreder@gmail.com>
2016-05-04 22:55:50 -0400

    completed ex 4 extra credit

--- ex4.c
+++ ex4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/* Warning: This program is wrong on purpose. */
+/* Warning: This program is fixed now. */
 
 int main()
 {
   int age = 10;
-  int height;
+  int height = 65;
 
-  printf("I am %d years old.\n");
+  printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
 
   return 0;
