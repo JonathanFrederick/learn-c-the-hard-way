9b7c13c393257ae4cdc23d541265d00e5eef5ad6
Jonathan Frederick <jrfreder@gmail.com>
2016-05-05 17:38:57 -0400

    completed exercise 5

---
+++ ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // imports standard input-output library
+
+/* This is a comment. */  //open and close a multi-line comment
+
+int main(int argc, char *argv[]) // main declaration with arguments
+{ // opens block for main function
+  int distance = 100;
+
+  // this is also a comment
+  printf("You are %d miles away.\n", distance);
+
+  return 0; // return to terminal
+} //closes block for main function
