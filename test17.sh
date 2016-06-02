f662a5c59cbaff5672c1f42f34600a81dae4e40f
Jonathan Frederick <jrfreder@gmail.com>
2016-06-01 23:48:14 -0400

    experimented with variables and if statements in shell script

--- test17.sh
+++ test17.sh
@@ -4,9 +4,30 @@ make
 ./ex17 test.dat c
 echo DATABASE CREATED
 
-./ex17 test.dat s 1 jon jon@email.com
-./ex17 test.dat s 2 bob bob@email.com
-./ex17 test.dat s 3 alex alex@email.com
+./ex17 test.dat s 1 jon jon@email.com | $a
+if [ "$a" = "" ]
+then
+  echo "ENTRY SET FOR JON"
+else
+  echo "FAILED SETTING ENTRY FOR JON"
+fi
+
+./ex17 test.dat s 2 bob bob@email.com | $a
+if [ "$a" = "" ]
+then
+  echo "ENTRY SET FOR BOB"
+else
+  echo "FAILED SETTING ENTRY FOR BOB"
+fi
+
+./ex17 test.dat s 3 alex alex@email.com | $a
+if [ "$a" = "" ]
+then
+  echo "ENTRY SET FOR ALEX"
+else
+  echo "FAILED SETTING ENTRY FOR ALEX"
+fi
+
 echo ENTRIES SET
 
 ./ex17 test.dat l
